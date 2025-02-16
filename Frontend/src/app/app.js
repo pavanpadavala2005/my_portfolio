a4c221f6ba71136000ec10ec0a5d19c2a4df0d94
pavanpadavala2005 <rokzpavan143@gmail.com>
2025-02-16 16:16:15 +0530

    comitting

--- Frontend/src/app/app.js
+++ Frontend/src/app/app.js
@@ -1,16 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import dataSlice from "../provider/dataSlice";
 
 export const store = configureStore({
 	reducer: {
 		data: dataSlice,
-=======
-import DataSlice from "../provider/DataSlice";
-
-export const store = configureStore({
-	reducer: {
-		data: DataSlice,
->>>>>>> ba8c7a792208094dbbd1719798c48fef89533169
 	},
 });
