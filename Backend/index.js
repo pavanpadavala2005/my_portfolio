a02f401f329496cdc73af6410e80887984be50a9
pavanpadavala2005 <rokzpavan143@gmail.com>
2025-02-17 13:50:52 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,21 +7,26 @@ import mailRoutes from "./routes/receiveMail.routes.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL, process.env.SECOND_FRONTEND_URL];
+app.use(cors({
+  origin: 'https://pavan-kumar-padavala.vercel.app'
+}));
 
-app.use(
-	cors({
-		origin: function (origin, callback) {
-			if (!origin || allowedOrigins.includes(origin)) {
-				callback(null, true);
-			} else {
-				callback(new Error("Not allowed by CORS"));
-			}
-		},
-		methods: ["POST", "GET", "PUT", "DELETE"],
-		credentials: true,
-	})
-);
+// const allowedOrigins = [process.env.FRONTEND_URL, process.env.SECOND_FRONTEND_URL];
+
+
+// app.use(
+// 	cors({
+// 		origin: function (origin, callback) {
+// 			if (!origin || allowedOrigins.includes(origin)) {
+// 				callback(null, true);
+// 			} else {
+// 				callback(new Error("Not allowed by CORS"));
+// 			}
+// 		},
+// 		methods: ["POST", "GET", "PUT", "DELETE"],
+// 		credentials: true,
+// 	})
+// );
 
 // app.use(
 // 	cors({
@@ -31,7 +36,6 @@ app.use(
 // 	})
 // );
 
-// app.use(cors());
 
 app.use(express.json());
 
